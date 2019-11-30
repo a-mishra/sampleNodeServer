333add4e77941f948e8a92ba3e2ab47655df414a
a-mishra <ashutosh.m812@gmail.com>
2019-11-30 19:36:37 +0530

    bug fix : on edit added settings updated : earlier it was just contacts update

--- src/controllers/settingController.js
+++ src/controllers/settingController.js
@@ -498,6 +498,25 @@ export const updateSetting_dt = async (req, res) => {
     var campaign_id;
     var defaultLeadId;
 
+    let setArray = [];
+    setArray["msg_type"] = req.body.msg_type;
+    setArray["prompt_name"] = req.body.prompt_name;
+    setArray["start_date"] = req.body.start_date;
+    setArray["end_date"] = req.body.end_date;
+    setArray["is_enabled"] = req.body.is_enabled;
+    setArray["priority"] = req.body.priority;
+    setArray["frequency"] = req.body.frequency;
+
+    sqlQuery = `update custom_functional_message_module_settings set ${setArray.join(', ')} where id = ${currentId}`;
+
+    try {
+        let SQLresult = await client.query(sqlQuery, values);
+    } catch (e) {
+        console.error(e.stack)
+    }
+
+
+
     // sqlQuery = `SELECT * FROM custom_functional_message_module_settings where id=${req.params.settingId}`;
     // console.log(sqlQuery);
     try {
